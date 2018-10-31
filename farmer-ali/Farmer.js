e865d230f222642113bc372669341e8326b02031
Ali Blackwell <aliblackwell@protonmail.com>
2018-10-31 10:48:35 +0000

    draw farmer as a lil tractor

--- farmer-ali/Farmer.js
+++ farmer-ali/Farmer.js
@@ -1,11 +1,14 @@
 class Farmer {
     constructor(userName) {
         this.userName = userName;
+        this.image = loadImage('farmer.png')
     }
 
     speak(words) {
         alert(words)
     }
 
-
+    draw() {
+        image(this.image, 0, 0, 60, 60)
+    }
 } // End of the Farmer class oooarrr
