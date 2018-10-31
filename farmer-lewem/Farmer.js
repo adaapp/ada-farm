13040732ed7d03b88f04851749c2b14c98879923
Emma Peakall <epeakall@gmail.com>
2018-10-31 15:42:10 +0000

    finished farm

--- farmer-lewem/Farmer.js
+++ farmer-lewem/Farmer.js
@@ -4,15 +4,35 @@ class Farmer {
 constructor(userName) {
 this.userName = userName;
 this.image = loadImage('farmer.png') 
+this.x = 100
+this.y = 100
+this.width = 80
+this.height = 80
 
 }
 
 speak(words) {
-alert(words)
+    alert(words)
+}
+
+update() {
+    if (keyIsDown(RIGHT_ARROW)) {
+        this.x = this.x + 3
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        this.x = this.x - 3
+}
+if (keyIsDown(UP_ARROW)) {
+    this.y = this.y - 3
+}
+if (keyIsDown(DOWN_ARROW)) {
+    this.y = this.y + 3
+}
 }
 
 draw() {
-    image(this.image,0,0, 80, 80)
+    var bobAmount = Math.sin(millis() / 100) * 5
+    image(this.image,this.x, this.y + bobAmount, this.width, this.height)
 }
 
 } // end of Farmer class  oooooaaarrrr
--- farmer-lewem/index.js
+++ farmer-lewem/index.js
@@ -1,25 +1,60 @@
 
 //Require is used instead of scripts anad is used when you are working on a server instead of a browser
 //Check node intro screencast
-var fieldA, fieldB, fieldC, fieldD;
+var fields = []
 var farmer
-function setup(){
+var activeField
+function setup() {
 
     createCanvas(window.innerWidth, window.innerHeight)
-    background(100,227,30)
+    background(50, 100, 70)
     alert("Welcome to the farm!")
     var username = prompt("What is your name?")
     console.log(username)
     farmer = new Farmer(username);
     farmer.speak("ey ih ey ih oh")
-    fieldA = new Field(1, 10)
-    fieldB = new Field(2, 10)
-    fieldC = new Field(3, 10)
-    fieldD = new Field(4, 10)
-    
+
+    fields.push(
+        new Field('pig field', 10, 25, 25, 300, 300)
+    )
+    fields.push(
+        new Field('tiger field', 10, 350, 25, 300, 300)
+    )
+    fields.push(
+        new Field('giraffe field', 10, 25, 350, 300, 300)
+    )
+
+    fields.push(
+        new Field('emma field', 10, 350, 350, 300, 300)
+    )
+
+
 }
 function draw() {
+    background(50, 100, 70)
+    for (var field of fields) {
+        field.draw()
+    }
+    farmer.update()
     farmer.draw()
+
+    setActiveFieldFromFarmerPosition()
+    if (activeField) {
+        noStroke()
+        fill('black')
+        textSize(15)
+        text(activeField.name, 0, 10)
+    }
 
 }
 
+function setActiveFieldFromFarmerPosition() {
+    activeField = null
+    for (var field of fields) {
+        if (field.containsPoint(farmer.x, farmer.y)) {
+            activeField = field
+        }
+
+
+    }
+}
